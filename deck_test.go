88221963d50c807c333a8d731794bd34b1d9696f
Djamaile Rahamat <rdjamaile@gmail.com>
2020-03-09 22:38:58 +0100

    added tests

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,35 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T){
 	d := newDeck()
 
 	if len(d) != 16 {
 		t.Errorf("Got wrong length, expected 16, got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d) - 1] != "Four of Clubs"{
+		t.Errorf("Expected Four of Clubs, but got %v", d[len(d) -1 ])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T){
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if loadedDeck.getLengthOfDeck() != 16{
+		t.Errorf("Got wrong length, expected 16, got %v", len(loadedDeck))
 	}
 }
